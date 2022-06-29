079916aa78d6757d6c9d42a4253b8d29f086dd90
MTuan247 <mtuan2472000@gmail.com>
2022-06-29 21:45:03 +0700

    Api Xu ly admin

---
+++ BComic/Core.Api/Controllers/Core/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using MySqlConnector;
+using System.Data;
+
+using Library.Models;
+using Microsoft.Extensions.Configuration;
+using Library.Enums;
+using Library.Interfaces;
+
+namespace Api.Controllers
+{
+    public class CategoryController : BaseController<Category>
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public CategoryController(ITServiceCollection serviceCollection) : base(serviceCollection)
+        {
+        }
+        #endregion
+
+        #region Methods
+        
+        #endregion
+    }
+}
--- BComic/Library/Models/Chapter.cs
+++ BComic/Library/Models/Chapter.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Library.Models
 {
+    [Table("Chapter")]
     public class Chapter
     {
         [ExplicitKey]
